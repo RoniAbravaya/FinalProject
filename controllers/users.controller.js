63c18344ce06b11dd518b23ccaa057f2dd5edf13
RoniAbravaya <150835357+RoniAbravaya@users.noreply.github.com>
2024-04-29 16:18:01 +0300

    update

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,5 +1,5 @@
 import { register, login, allusers } from "../models/users.model.js";
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
